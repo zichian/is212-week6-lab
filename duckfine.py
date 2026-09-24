abe28f0be45cd201252f023f24ba517fdd169ff2
zichian <zc.wong.2024@computing.smu.edu.sg>
2026-09-25 00:47:00 +0800

    fixed code

--- duckfine.py
+++ duckfine.py
@@ -2,7 +2,7 @@ class DuckFine:
     """Late fees for the QuackLoan rubber-duck lending library."""
     DAILY_FEE = 0.50  # dollars per chargeable day
     GRACE_DAYS = 2  # the first two days late are forgiven
-    MAX_FEE = 0.00  # a single fine never exceeds this
+    MAX_FEE = 5.00  # a single fine never exceeds this
 
     def __init__(self, member_id):
         self.member_id = member_id
